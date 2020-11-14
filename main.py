6771c5022d8f35014a23772fed3db79f8855d333
Adrian Richter <adrichter97@gmail.com>
2020-11-14 20:14:55 +0100

    add flask route methods for REST

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import rest
 import sql
-from datetime import datetime
 
 if __name__ == '__main__':
     sql.init()
 
-    xd = sql.post_news({'timestamp': datetime(1997, 11, 19), 'title': 'Hello there', 'message': 'IM ALIVE!'})
-    xd = sql.post_news({'timestamp': datetime(1997, 11, 20), 'title': 'Hello there1', 'message': 'IM ALIVE!'})
-    xd = sql.post_news({'timestamp': datetime.now(), 'title': 'Hello there2', 'message': 'IM ALIVE!'})
-    xd = sql.get_news_id(1)
-    print(xd)
+    # sql.delete_news_id(6)
+
+    rest.serve()
+
+    # xd = sql.post_news({'timestamp': datetime(1997, 11, 19), 'title': 'Hello there', 'message': 'IM ALIVE!'})
+    # xd = sql.post_news({'timestamp': datetime(1997, 11, 20), 'title': 'Hello there1', 'message': 'IM ALIVE!'})
+    # xd = sql.post_news({'timestamp': datetime.now(), 'title': 'Hello there2', 'message': 'IM ALIVE!'})
+    # xd = sql.get_news_id(1)
+    # print(xd)
--- rest.py
+++ rest.py
@@ -1,4 +1,9 @@
-from flask import Flask, jsonify
+from datetime import datetime
+
+from flask import Flask, jsonify, request
+
+import error
+import sql
 
 app = Flask(__name__)
 
@@ -7,6 +12,101 @@ def serve():
     app.run(port=26548, debug=True)
 
 
-@app.route('/test')
-def get_test():
-    return jsonify(['this', 42, 'nice', 3.14])
+# @app.route('/test', methods=['GET'])
+# def get_test():
+#     return jsonify(['this', 42, 'nice', 3.14])
+
+
+@app.route('/news', methods=['GET'])
+def get_news():
+    try:
+        data = sql.get_news()
+        code = 200
+    except error.DBError:
+        data = []
+        code = 500
+
+    return jsonify(data), code
+
+
+@app.route('/news/<int:id>', methods=['GET'])
+def get_news_id(id):
+    try:
+        data = sql.get_news_id(id)
+        code = 200
+    except error.NotFoundError:
+        data = {}
+        code = 404
+    except error.DBError:
+        data = {}
+        code = 500
+
+    return jsonify(data), code
+
+
+@app.route('/news', methods=['POST'])
+def post_news():
+    data, code = validate_request()
+    if code != 202:
+        return data, code
+
+    try:
+        data = sql.post_news(data)
+        code = 201
+    except error.NotFoundError:
+        data = {}
+        code = 404
+    except error.DBError:
+        data = {}
+        code = 500
+
+    return jsonify(data), code
+
+
+@app.route('/news/<int:id>', methods=['PUT'])
+def put_news_id(id):
+    data, code = validate_request()
+    if code != 202:
+        return data, code
+
+    try:
+        data = sql.put_news_id(id, data)
+        code = 200
+    except error.NotFoundError:
+        data = {}
+        code = 404
+    except error.DBError:
+        data = {}
+        code = 500
+
+    return jsonify(data), code
+
+
+@app.route('/news/<int:id>', methods=['DELETE'])
+def delete_news_id(id):
+    try:
+        data = sql.delete_news_id(id)
+        code = 200
+    except error.NotFoundError:
+        data = {}
+        code = 404
+    except error.DBError:
+        data = {}
+        code = 500
+
+    return jsonify(data), code
+
+
+def validate_request():
+    if not request.is_json:
+        return {}, 415
+    data = request.get_json(silent=True)
+    if not data:
+        return {}, 400
+    if not isinstance(data, dict):
+        return {}, 400
+    if 'title' not in data or 'message' not in data:
+        return {}, 400
+    if 'timestamp' not in data:
+        data['timestamp'] = datetime.now()
+    return data, 202
