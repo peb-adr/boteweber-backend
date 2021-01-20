7a5330ae366e6a10dcd8db66ce2c05dfec5b2a59
Adrian Richter <adrichter97@gmail.com>
2021-01-20 16:32:07 +0100

    add subscriber and group routes

--- src/rest.py
+++ src/rest.py
@@ -25,6 +25,255 @@ def serve():
 
 
 #
+# TEST
+#
+
+
+@app.route('/test', methods=['GET'])
+def get_test():
+    try:
+        data = sql.select('subscriber')
+        for i in range(0, len(data)):
+            data[i] = schema.convert_instance_formatted_properties_to_json('subscriber', data[i])
+        code = 200
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+@app.route('/test', methods=['POST'])
+def post_test():
+    data, code = validate_request()
+    if code != 202:
+        return make_response(jsonify(data), code)
+
+    try:
+        schema.validate('subscriber', data)
+    except error.ValidationError as e:
+        return make_response(jsonify(make_error_data(e)), 400)
+
+    try:
+        data = schema.convert_instance_formatted_properties_from_json('subscriber', data)
+        data = sql.insert('subscriber', data)
+        data = schema.convert_instance_formatted_properties_to_json('subscriber', data)
+        code = 201
+    except error.NotFoundError as e:
+        data = make_error_data(e)
+        code = 404
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+#
+# SUBSCRIBERS
+#
+
+
+@app.route('/subscribers', methods=['GET'])
+def get_subscribers():
+    try:
+        data = sql.select('subscriber')
+        for i in range(0, len(data)):
+            data[i] = schema.convert_instance_formatted_properties_to_json('subscriber', data[i])
+        code = 200
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+@app.route('/subscribers/<int:id>', methods=['GET'])
+def get_subscribers_id(id):
+    try:
+        data = sql.select_by_id('subscriber', id)
+        data = schema.convert_instance_formatted_properties_to_json('subscriber', data)
+        code = 200
+    except error.NotFoundError as e:
+        data = make_error_data(e)
+        code = 404
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+@app.route('/subscribers', methods=['POST'])
+def post_subscribers():
+    data, code = validate_request()
+    if code != 202:
+        return make_response(jsonify(data), code)
+
+    try:
+        schema.validate('subscriber', data)
+    except error.ValidationError as e:
+        return make_response(jsonify(make_error_data(e)), 400)
+
+    try:
+        data = schema.convert_instance_formatted_properties_from_json('subscriber', data)
+        data = sql.insert('subscriber', data)
+        data = schema.convert_instance_formatted_properties_to_json('subscriber', data)
+        code = 201
+    except error.NotFoundError as e:
+        data = make_error_data(e)
+        code = 404
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+@app.route('/subscribers/<int:id>', methods=['PUT'])
+def put_subscribers_id(id):
+    data, code = validate_request()
+    if code != 202:
+        return make_response(jsonify(data), code)
+
+    try:
+        schema.validate('subscriber', data)
+    except error.ValidationError as e:
+        return make_response(jsonify(make_error_data(e)), 400)
+
+    try:
+        data = schema.convert_instance_formatted_properties_from_json('subscriber', data)
+        data = sql.update_by_id('subscriber', id, data)
+        data = schema.convert_instance_formatted_properties_to_json('subscriber', data)
+        code = 200
+    except error.NotFoundError as e:
+        data = make_error_data(e)
+        code = 404
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+@app.route('/subscribers/<int:id>', methods=['DELETE'])
+def delete_subscribers_id(id):
+    try:
+        data = sql.delete_by_id('subscriber', id)
+        code = 200
+    except error.NotFoundError as e:
+        data = make_error_data(e)
+        code = 404
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+#
+# GROUPS
+#
+
+
+@app.route('/groups', methods=['GET'])
+def get_groups():
+    try:
+        data = sql.select('group')
+        for i in range(0, len(data)):
+            data[i] = schema.convert_instance_formatted_properties_to_json('group', data[i])
+        code = 200
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+@app.route('/groups/<int:id>', methods=['GET'])
+def get_groups_id(id):
+    try:
+        data = sql.select_by_id('group', id)
+        data = schema.convert_instance_formatted_properties_to_json('group', data)
+        code = 200
+    except error.NotFoundError as e:
+        data = make_error_data(e)
+        code = 404
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+@app.route('/groups', methods=['POST'])
+def post_groups():
+    data, code = validate_request()
+    if code != 202:
+        return make_response(jsonify(data), code)
+
+    try:
+        schema.validate('group', data)
+    except error.ValidationError as e:
+        return make_response(jsonify(make_error_data(e)), 400)
+
+    try:
+        data = schema.convert_instance_formatted_properties_from_json('group', data)
+        data = sql.insert('group', data)
+        data = schema.convert_instance_formatted_properties_to_json('group', data)
+        code = 201
+    except error.NotFoundError as e:
+        data = make_error_data(e)
+        code = 404
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+@app.route('/groups/<int:id>', methods=['PUT'])
+def put_groups_id(id):
+    data, code = validate_request()
+    if code != 202:
+        return make_response(jsonify(data), code)
+
+    try:
+        schema.validate('group', data)
+    except error.ValidationError as e:
+        return make_response(jsonify(make_error_data(e)), 400)
+
+    try:
+        data = schema.convert_instance_formatted_properties_from_json('group', data)
+        data = sql.update_by_id('group', id, data)
+        data = schema.convert_instance_formatted_properties_to_json('group', data)
+        code = 200
+    except error.NotFoundError as e:
+        data = make_error_data(e)
+        code = 404
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+@app.route('/groups/<int:id>', methods=['DELETE'])
+def delete_groups_id(id):
+    try:
+        data = sql.delete_by_id('group', id)
+        code = 200
+    except error.NotFoundError as e:
+        data = make_error_data(e)
+        code = 404
+    except error.DBError as e:
+        data = make_error_data(e)
+        code = 500
+
+    return make_response(jsonify(data), code)
+
+
+#
 # NEWS
 #
 
@@ -171,6 +420,13 @@ def put_info():
         code = 500
 
     return make_response(jsonify(data), code)
+
+
+def resolve_relations(data, schema_name):
+    d = schema.get(schema_name)
+    if not d:
+        return
+    pass
 
 
 def validate_request():
